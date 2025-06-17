70ac9bb3837803d6b981fe6ec7464054bf63b69a
marcusmi4n <marcuslutas@gmail.com>
2025-06-17 09:51:55 +0300

    testing supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
 
   try {
-    await dotenv.load(fileName: ".env");
-    print("✅ .env loaded successfully");
+    await Supabase.initialize(
+      url: dotenv.env['SUPABASE_URL']!,
+      anonKey: dotenv.env['SUPABASE_KEY']!,
+    );
+    print("✅ Supabase initialized");
   } catch (e) {
-    print("❌ .env failed: $e");
+    print("❌ Supabase failed: $e");
   }
 
   runApp(MaterialApp(
     home: Scaffold(
-      appBar: AppBar(title: Text("Zeyquorra - Step 1")),
-      body: Center(child: Text("Dotenv loaded")),
+      appBar: AppBar(title: Text("Zeyquorra - Step 2")),
+      body: Center(child: Text("Supabase initialized")),
     ),
   ));
 }
